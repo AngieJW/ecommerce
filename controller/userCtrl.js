290a92c61194b789e84d4ca435bc7398bdf5799f
Angie Duhard <angie.duhard@gmail.com>
2023-09-22 14:34:29 +0200

    start password error handling

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,18 +1,17 @@
 const User = require('../models/userModel');
+const asyncHandler = require('express-async-handler');
 
-const createUser = async (req, res) => {
+const createUser = asyncHandler(async (req, res) => {
   const email = req.body.email;
   const findUser = await User.findOne({ email: email });
   if (!findUser) {
     // create new user
-    const newUser = User.create(req.body);
+    const newUser = await User.create(req.body);
     res.json(newUser);
   } else {
-    res.json({
-        message: 'User already exists',
-        success: false,
-      });
+    res.status(400);
+    throw new Error('User already exists');
   }
-}
+});
 
 module.exports = { createUser };
